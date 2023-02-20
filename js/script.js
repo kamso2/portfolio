2c6d1766744bdf8790cf3c7c5fb5be5a615f48b0
kamso <kamso1805@gmail.com>
2023-02-20 12:55:53 +0000

    site ok dois mettre a jour le contenu

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const paraCouleur = document.querySelector(".para");
 paraCouleur.addEventListener("click",() =>{ 
 document.querySelector(".theme-bar").classList.toggle("open")});
 
-// transition du bouton se ferme quand on scroll
+// transition du bouton se ferme quand on scroll et quand on click
 window.addEventListener("scroll", () => {
     if (document.querySelector(".theme-bar").classList.contains("open")) {
         document.querySelector(".theme-bar").classList.remove("open");
@@ -18,6 +18,7 @@ document.querySelector(".contenu").addEventListener("click",() => {
 
 // bouton nuit jour change quand le theme change
 const nuitJour = document.querySelector(".nuit-jour");
+var theme;
 
 nuitJour.addEventListener("click",() =>{
     if (document.body.classList.contains("dark")) {
@@ -25,49 +26,54 @@ nuitJour.addEventListener("click",() =>{
         nuitJour.querySelector("i").classList.add("fa-moon");
         document.body.classList.remove("dark");
         document.body.classList.add("light");
+        theme = "light";
 
     } else if (document.body.classList.contains("light")) {
         nuitJour.querySelector("i").classList.remove("fa-moon");
         nuitJour.querySelector("i").classList.add("fa-sun");
         document.body.classList.remove("light");
         document.body.classList.add("dark");
+        theme = "dark";
        
     }
+    localStorage.setItem("pageTheme", JSON.stringify(theme));
+    
 })
 
+// pour sauvegarder les themes quand on change le mode dark/light
+let getTheme = JSON.parse(localStorage.getItem("pageTheme"));
+
+if (getTheme === "light") {
+    document.body.classList = "light";
+} else if (getTheme === "dark") {
+    document.body.classList = "dark";
+}
 
 
 // changement de theme
 
 const themeCouleur = document.querySelectorAll(".themeCouleur");
+// var couleur;
 function setActive(couleur) {
     themeCouleur.forEach((style) => {
         if (couleur === style.getAttribute("title")) {
-            style.removeAttribute("disabled");
+            style.removeAttribute("disabled");    
         } else {
-            style.setAttribute("disabled","true");
+            style.setAttribute("disabled","true");    
         }
+        localStorage.setItem("change", JSON.stringify(couleur));
     } )
 }
 
-// const changeTheme = document.querySelector(".theme");
-// const couleur1 = document.querySelector(".couleur1");
-// const couleur2 = document.querySelector(".couleur2");
-// const couleur3 = document.querySelector(".couleur3");
-// const couleur4 = document.querySelector(".couleur4");
-
-// couleur1.addEventListener("click", () =>{
-//     document.documentElement.style.setProperty("--theme-color1-900", "rgb(255, 0, 0)" );
-// })
-// couleur2.addEventListener("click", () =>{
-//     document.documentElement.style.setProperty("--theme-color1-900", "rgb(0, 165, 11)" );
-// })
-// couleur3.addEventListener("click", () =>{
-//     document.documentElement.style.setProperty("--theme-color1-900", "rgb(0, 107, 247)" );
-// })
-// couleur4.addEventListener("click", () =>{
-//     document.documentElement.style.setProperty("--theme-color1-900", "rgb(225, 124, 2)" );
-// })
-
-
-
+// pour sauvegarder les theme quand on change de page
+let getCouleur = JSON.parse(localStorage.getItem("change"));
+console.log(getCouleur);
+if (getCouleur === "couleur1") {
+    document.querySelector(".C1").removeAttribute("disabled");
+}else if (getCouleur === "couleur2") {
+    document.querySelector(".C2").removeAttribute("disabled");
+}else if (getCouleur === "couleur3") {
+    document.querySelector(".C3").removeAttribute("disabled");
+}else if (getCouleur === "couleur4") {
+    document.querySelector(".C4").removeAttribute("disabled");
+}
